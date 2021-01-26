338f7acf1d057a405927191683bf9ae0fdccee64
Colin Maresh <cmaresh57@gmail.com>
2021-01-26 14:06:00 -0600

    option tray styles added

--- list.php
+++ list.php
@@ -78,7 +78,32 @@ $conn->close();
 
 <div class="backdrop birds"></div>
 <section id="lists">
-    <div class="container padded"><div class="row no-gutters">
+    <div class="container padded">
+        <?php if ($owner): ?>
+            <div class="list-options-pos">
+                <div class="list-options">
+                    <div class="list-option add">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+                    </div>
+                    <div class="list-option cancel" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
+                    </div>
+                    <div class="list-option shiftup" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>
+                    </div>
+                    <div class="list-option shiftdown" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>
+                    </div>
+                    <div class="list-option changepos" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M7 11.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/></svg>
+                    </div>
+                    <div class="list-option remove" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="row no-gutters">
         <?php if (!$exists): ?>
         <div class="col-12">
             <div id="no-access">
@@ -148,14 +173,27 @@ $conn->close();
                 </li>-->
                 </ol>
             </div>
-            <?php if ($owner): ?>
-            <div class="list-options">
-                <div class="list-option add"><h6>+</h6></div>
-                <div class="list-option remove"><h6>-</h6></div>
-            </div>
-            <?php endif; ?>
         </div>
         <div class="col-md-6">
+            <div class="center-col">
+                <div class="desktop-movie-wrap">
+                    <div class="poster-section">
+                        <img class="poster-blurfix" src="./media/shapeofwater.jpg" alt="poster" />
+                        <img class="poster-background" src="./media/shapeofwater.jpg" alt="poster" />
+                        <img class="poster-image" src="./media/shapeofwater.jpg" alt="poster" />
+                    </div>
+                    <div class="movie-info">
+                        <h7 class="movie-title">The Shape of Water</h7><br>
+                        <span class="movie-subtext">R | 2h 3m | 22 December 2017</span><br>
+                        <b>Director: </b>Guillermo del Toro<br>
+                        <b>Writers: </b>Guillermo del Toro, Venessa Taylor<br>
+                        <b>Stars: </b>Sally Hawkins, Octavia Spencer, Michael Shannon<br>
+                    </div>
+                    <div class="close-item">
+                        ^
+                    </div>
+                </div>
+            </div>
         </div>
         <?php endif; ?>
     </div></div></div>
@@ -227,13 +265,13 @@ $conn->close();
 
         $('.list-name').click(function() {
             $(this).toggleClass('selected');
-            $('.selected').length > 0 ? $('.remove').show() : $('.remove').hide();
+            $('.selected').length > 0 ? $('.remove').css("display", "flex") : $('.remove').hide();
         });
 
         $('#lock').click(function() {
             $.post('/sparrow/api/togglelock.php', { id: <?php echo $listId; ?> });
             $(this).toggleClass('locked');
-        })
+        });
     });
 </script>
 <?php endif; ?>
