0aaf3fd5390bf77a567fbe45983e281b60d72aab
cmaresh <cmaresh57@gmail.com>
2021-01-28 19:25:18 -0600

    plume outline

---
+++ scripts/plume.js
@@ -0,0 +1,13 @@
+//when document is ready
+//  PLUME = find element with [plume]
+//  
+//  if no elements have [plume], return
+//
+//  generate menu elements (including listeners)
+//  check if <plume-menu /> exists
+//  if it does:
+//      add menu elements as child of <plume-menu />
+//  else:
+//      add menu elements as child of <body>
+//
+//  
