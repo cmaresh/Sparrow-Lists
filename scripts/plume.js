b7991e4587c865311809e3df7d98ae6bb17da834
cmaresh <cmaresh57@gmail.com>
2021-01-29 21:33:41 -0600

    plume checkpoint

--- scripts/plume.js
+++ scripts/plume.js
@@ -37,7 +37,7 @@
 // </div>
 
 function defaultAdd(menu) {
-    menu.toggleSubmenu('adding');
+    menu.toggleSubmenu('selection');
 }
 
 function defaultCancel(menu) {
@@ -56,8 +56,36 @@ function defaultChangePos(menu) {
 
 }
 
+function defaultEdit(menu) {
+
+}
+
 function defaultRemove(menu) {
 
+}
+
+class List {
+    newItem =   <template>
+                    <li>
+                        <input type="text"></input>
+                        <div plume-autocomplete></div>
+                    </li>
+                </template>;
+    item = <template><li></li></template>;
+
+    constructor(list) {
+        this.elem = list;
+        this.items = [];
+
+        for (let i = 0; i < list.children.length; i++) {
+            list.children[i].setAttribute('plume-index', i);
+            this.items.push(list.children[i]);
+        }
+    }
+
+    addItem() {
+        this.elem.appendChild()
+    }
 }
 
 class Menu {
@@ -100,25 +128,31 @@ class Menu {
         }
 
         if (id == 'shiftup') {
-            newOption.submenu = 'editing';
+            newOption.submenu = 'selection';
             newOption.action = defaultShiftUp;
             newOption.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>';
         }
 
         if (id == 'shiftdown') {
-            newOption.submenu = 'editing';
+            newOption.submenu = 'selection';
             newOption.action = defaultShiftDown;
             newOption.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>';
         }
 
         if (id == 'changepos') {
-            newOption.submenu = 'editing';
+            newOption.submenu = 'selection';
             newOption.action = defaultChangePos;
             newOption.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M7 11.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/></svg>';
         }
 
+        if (id == 'edit') {
+            newOption.submenu = 'selection';
+            newOption.action = defaultEdit;
+            newOption.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16"><path d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>'
+        }
+
         if (id == 'remove') {
-            newOption.submenu = 'editing';
+            newOption.submenu = 'selection';
             newOption.action = defaultRemove;
             newOption.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>';
         }
@@ -173,13 +207,13 @@ class Menu {
  * the user in the injected menu. Possible actions are 'add', 'shift',
  * 'changepos' and 'delete'.
  */
-function plume(params=['add', 'cancel', 'shiftup', 'shiftdown', 'changepos', 'remove']) {
+function plume(params=['add', 'cancel', 'shiftup', 'shiftdown', 'changepos', 'edit', 'remove']) {
     // let creatingItem = false;
     // let editing = false;
     // let editingElem;
 
     document.addEventListener('DOMContentLoaded', (event) => {
-        const plume = document.querySelector('[plume]');
+        const listElem = document.querySelector('[plume]');
         const menuElem = document.querySelector('[plume-menu]');
 
         if (!plume) {
@@ -192,6 +226,7 @@ function plume(params=['add', 'cancel', 'shiftup', 'shiftdown', 'changepos', 're
             menuElem.setAttribute('plume-menu', '');
         }
 
+        const list = new List(listElem);
         const menu = new Menu(menuElem);
 
         params.forEach(param => menu.addOption(param));
